41b83add736d64ba433ca6338309ee302c4bc0a6
princelewis <princelewis4christ@yahoo.com>
2019-12-22 15:01:02 +0100

    update card statistics response structure

--- src/main/java/com/lewis/cardverification/response/StatResponse.java
+++ src/main/java/com/lewis/cardverification/response/StatResponse.java
@@ -1,4 +1,24 @@
 package com.lewis.cardverification.response;
 
-public class StatResponse {
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Map;
+
+@Data
+public class StatResponse implements Serializable {
+    private boolean success;
+    private int start;
+    private int limit;
+    private int size;
+    private Map<String, Object> payload;
+
+    public StatResponse(boolean success, int start, int limit, int size, Map<String, Object> payload) {
+        this.success = success;
+        this.start = start;
+        this.limit = limit;
+        this.size = size;
+        this.payload = payload;
+    }
+
 }
