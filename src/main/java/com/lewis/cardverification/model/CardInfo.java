a7c92a9b433d417201e2021757cb1efdbb644e1a
princelewis <princelewis4christ@yahoo.com>
2019-12-22 13:55:26 +0100

    create an entity class

--- src/main/java/com/lewis/cardverification/model/CardInfo.java
+++ src/main/java/com/lewis/cardverification/model/CardInfo.java
@@ -1,4 +1,37 @@
 package com.lewis.cardverification.model;
 
-public class CardInfo {
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "card_info")
+public class CardInfo implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "card_type")
+    private String cardType;
+    private String scheme;
+    private String bank;
+
+    @Column(name="card_number")
+    private String cardNumber;
+    private boolean success;
+
+    public CardInfo() {
+    }
+
+    public CardInfo(String cardType, String scheme, String bank, String cardNumber, boolean success) {
+        this.cardType = cardType;
+        this.scheme = scheme;
+        this.bank = bank;
+        this.cardNumber = cardNumber;
+        this.success = success;
+    }
+
 }
