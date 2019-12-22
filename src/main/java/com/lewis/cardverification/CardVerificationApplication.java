c1807f21f750c23929c61da7c1f803234a61a4d7
princelewis <princelewis4christ@yahoo.com>
2019-12-22 12:26:47 +0100

    instatiate a spring-boot project

---
+++ src/main/java/com/lewis/cardverification/CardVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.lewis.cardverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardVerificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardVerificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lewis/cardverification/CardVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewis.cardverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardVerificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
