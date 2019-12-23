91ea23e2d0c61312686bb5b3240672f4dd124ef6
princelewis <princelewis4christ@yahoo.com>
2019-12-23 19:22:54 +0100

    create a mysql query to group and count the occurence of card numbers in the database

--- src/main/java/com/lewis/cardverification/repository/CardInfoRepository.java
+++ src/main/java/com/lewis/cardverification/repository/CardInfoRepository.java
@@ -2,9 +2,15 @@ package com.lewis.cardverification.repository;
 
 import com.lewis.cardverification.model.CardInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CardInfoRepository extends JpaRepository<CardInfo, Long> {
     CardInfo findByCardNumber(String cardNumber);
+
+    @Query("SELECT COUNT (cardInfo.success), cardInfo.cardNumber FROM CardInfo  cardInfo GROUP BY cardInfo.cardNumber ORDER BY COUNT (cardInfo.success) DESC ")
+    List<List> sortByCardNumber();
 }
