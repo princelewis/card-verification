3e5b18d9944b149f619b64401b7659e450946d42
princelewis <princelewis4christ@yahoo.com>
2019-12-23 19:17:49 +0100

    Implement cardInfo method to extract the response from the third-party API and respond with a customised response structure

--- src/main/java/com/lewis/cardverification/service/CardInfoServiceImpl.java
+++ src/main/java/com/lewis/cardverification/service/CardInfoServiceImpl.java
@@ -1,11 +1,19 @@
 package com.lewis.cardverification.service;
 
+import com.lewis.cardverification.exception.CardInfoServiceException;
+import com.lewis.cardverification.exception.ErrorMessages;
+import com.lewis.cardverification.model.CardInfo;
+import com.lewis.cardverification.pojo.ResponsePojo;
 import com.lewis.cardverification.repository.CardInfoRepository;
 import com.lewis.cardverification.response.InfoResponse;
 import com.lewis.cardverification.response.StatResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class CardInfoServiceImpl implements CardInfoService {
@@ -31,4 +39,35 @@ public class CardInfoServiceImpl implements CardInfoService {
         return cardStat(start, limit);
     }
 
+    private InfoResponse cardInfo(String cardNumber) throws RestClientException {
+
+        if (cardNumber.length() < 6) {
+            throw new CardInfoServiceException(ErrorMessages.INCOMPLETE_CARD_DIGIT.getErrorMessage());
+        }
+
+        String newCardNumber = cutOutFirstSixChar(cardNumber);
+
+        Map<String, Object> payload = new HashMap<>();
+
+
+        ResponsePojo thirdPartyApiResponse = restTemplate.getForObject("https://lookup.binlist.net/" + newCardNumber, ResponsePojo.class);
+
+        if(thirdPartyApiResponse == null){
+            throw new CardInfoServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
+        }
+
+        CardInfo cardInfoEntity = new CardInfo(thirdPartyApiResponse.getType(), thirdPartyApiResponse.getScheme(), thirdPartyApiResponse.getBank().getName(), newCardNumber, true);
+
+        CardInfo savedDetails = cardInfoRepository.save(cardInfoEntity);
+
+        payload.put("scheme", savedDetails.getScheme());
+        payload.put("type", savedDetails.getCardType());
+        payload.put("bank", savedDetails.getBank());
+
+        return new InfoResponse(true, payload);
+    }
+
+    private String cutOutFirstSixChar(String cardNumber){
+        return cardNumber.substring(0,6);
+    }
 }
