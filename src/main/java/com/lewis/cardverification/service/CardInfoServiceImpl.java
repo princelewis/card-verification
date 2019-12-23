aabf0aa7f824e293b1543727b3315d1693d39c18
princelewis <princelewis4christ@yahoo.com>
2019-12-23 15:29:25 +0100

    implement methods inherited from CardInfoService interface

--- src/main/java/com/lewis/cardverification/service/CardInfoServiceImpl.java
+++ src/main/java/com/lewis/cardverification/service/CardInfoServiceImpl.java
@@ -1,5 +1,8 @@
 package com.lewis.cardverification.service;
 
+import com.lewis.cardverification.repository.CardInfoRepository;
+import com.lewis.cardverification.response.InfoResponse;
+import com.lewis.cardverification.response.StatResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,4 +19,16 @@ public class CardInfoServiceImpl implements CardInfoService {
         this.cardInfoRepository = cardInfoRepository;
         this.restTemplate = restTemplate;
     }
+
+    @Override
+    public InfoResponse getCardInfo(String cardNumber) {
+
+        return cardInfo(cardNumber);
+    }
+
+    @Override
+    public StatResponse getStat(int start, int limit) {
+        return cardStat(start, limit);
+    }
+
 }
