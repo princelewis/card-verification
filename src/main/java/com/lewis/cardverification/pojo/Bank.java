be587ceae803bdbc4992ebee52b8f3c24cd8369d
princelewis <princelewis4christ@yahoo.com>
2019-12-23 19:13:19 +0100

    create Bank class to store the bank data that would be received from the third-party API

---
+++ src/main/java/com/lewis/cardverification/pojo/Bank.java
@@ -0,0 +1,33 @@
+package com.lewis.cardverification.pojo;
+
+public class Bank {
+
+    private String name;
+    private String url;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
